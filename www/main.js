806d55ac0933f5d4c145bc48b07ec2ef1a6ef229
Miguel <146232277+mikeMigUelSg@users.noreply.github.com>
2025-04-14 13:09:29 +0100

    circulos bottom bar added

--- www/main.js
+++ www/main.js
@@ -3,21 +3,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Estimar valores médios para Android
     const topInset = 24;    // status bar (pode variar)
-    const bottomInset = 0; // barra de navegação (pode variar)
+    const bottomInset = 15; // barra de navegação (pode variar)
 
     container.style.paddingTop = `${topInset}px`;
     container.style.paddingBottom = `${bottomInset}px`;
     if (window.Capacitor && window.Capacitor.Plugins && window.Capacitor.Plugins.SplashScreen) {
         window.Capacitor.Plugins.SplashScreen.hide();
       }
+      
 
   });
   
 
-
-  document.querySelectorAll('.nav-item').forEach(item => {
-    item.addEventListener('click', () => {
-      document.querySelectorAll('.nav-item').forEach(i => i.classList.remove('active'));
-      item.classList.add('active');
+  document.addEventListener('DOMContentLoaded', () => {
+    const navItems = document.querySelectorAll('.bottom-nav .nav-item');
+  
+    navItems.forEach(item => {
+      item.addEventListener('click', () => {
+        navItems.forEach(i => i.classList.remove('active'));
+        item.classList.add('active');
+      });
     });
   });
+  
+
